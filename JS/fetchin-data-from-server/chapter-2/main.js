4b7d903214d6a0be3d9eb084c3b8c3d4a950121e
rutkaykarabulak <rutkaykarabulak@gmail.com>
2023-10-18 23:55:33 +0300

    Returning the original version from MDN

--- JS/fetchin-data-from-server/chapter-2/main.js
+++ JS/fetchin-data-from-server/chapter-2/main.js
@@ -1,83 +1,178 @@
-const main = document.querySelector("main");
-const request = new Request("./products.json");
-const select = document.querySelector("#category");
-// Fetch the products from @file products.json
-const promise = fetch(request);
-promise
-.then(response => {
+// use fetch to retrieve the products and pass them to init
+// report any errors that occur in the fetch operation
+// once the products have been successfully loaded and formatted as a JSON object
+// using response.json(), run the initialize() function
+fetch('products.json')
+  .then( response => {
     if (!response.ok) {
-        throw new Error("An error occured during network call");
+      throw new Error(`HTTP error: ${response.status}`);
     }
     return response.json();
-})
-.then(products => {
-    initialize(products);
-})
-.catch(error => {
-    console.log("An error occured: " + error);
-})
+  })
+  .then( json => initialize(json) )
+  .catch( err => console.error(`Fetch problem: ${err.message}`) );
 
-// Creates card in main section for each product in products
+// sets up the app logic, declares required variables, contains all the other functions
 function initialize(products) {
-    products.forEach(product => {
-        createItem(product);
-    })
-}
-// array that stores all created product elements
-let allProductSections = [];
+  // grab the UI elements that we need to manipulate
+  const category = document.querySelector('#category');
+  const searchTerm = document.querySelector('#searchTerm');
+  const searchBtn = document.querySelector('button');
+  const main = document.querySelector('main');
 
-function createItem(product) {
-    // create new elements
-    const h2 = document.createElement("h2");
-    const img = document.createElement("img");
-    const p = document.createElement("p");
-    const section = document.createElement("section");
-    
-    const photoURL = `./images/${product.image}`;
-    fetch(photoURL)
-    .then(response => {
+  // keep a record of what the last category and search term entered were
+  let lastCategory = category.value;
+  // no search has been made yet
+  let lastSearch = '';
+
+  // these contain the results of filtering by category, and search term
+  // finalGroup will contain the products that need to be displayed after
+  // the searching has been done. Each will be an array containing objects.
+  // Each object will represent a product
+  let categoryGroup;
+  let finalGroup;
+
+  // To start with, set finalGroup to equal the entire products database
+  // then run updateDisplay(), so ALL products are displayed initially.
+  finalGroup = products;
+  updateDisplay();
+
+  // Set both to equal an empty array, in time for searches to be run
+  categoryGroup = [];
+  finalGroup = [];
+
+  // when the search button is clicked, invoke selectCategory() to start
+  // a search running to select the category of products we want to display
+  searchBtn.addEventListener('click', selectCategory);
+
+  function selectCategory(e) {
+    // Use preventDefault() to stop the form submitting — that would ruin
+    // the experience
+    e.preventDefault();
+
+    // Set these back to empty arrays, to clear out the previous search
+    categoryGroup = [];
+    finalGroup = [];
+
+    // if the category and search term are the same as they were the last time a
+    // search was run, the results will be the same, so there is no point running
+    // it again — just return out of the function
+    if (category.value === lastCategory && searchTerm.value.trim() === lastSearch) {
+      return;
+    } else {
+      // update the record of last category and search term
+      lastCategory = category.value;
+      lastSearch = searchTerm.value.trim();
+      // In this case we want to select all products, then filter them by the search
+      // term, so we just set categoryGroup to the entire JSON object, then run selectProducts()
+      if (category.value === 'All') {
+        categoryGroup = products;
+        selectProducts();
+      // If a specific category is chosen, we need to filter out the products not in that
+      // category, then put the remaining products inside categoryGroup, before running
+      // selectProducts()
+      } else {
+        // the values in the <option> elements are uppercase, whereas the categories
+        // store in the JSON (under "type") are lowercase. We therefore need to convert
+        // to lower case before we do a comparison
+        const lowerCaseType = category.value.toLowerCase();
+        // Filter categoryGroup to contain only products whose type includes the category
+        categoryGroup = products.filter( product => product.type === lowerCaseType );
+
+        // Run selectProducts() after the filtering has been done
+        selectProducts();
+      }
+    }
+  }
+
+  // selectProducts() Takes the group of products selected by selectCategory(), and further
+  // filters them by the tiered search term (if one has been entered)
+  function selectProducts() {
+    // If no search term has been entered, just make the finalGroup array equal to the categoryGroup
+    // array — we don't want to filter the products further.
+    if (searchTerm.value.trim() === '') {
+      finalGroup = categoryGroup;
+    } else {
+      // Make sure the search term is converted to lower case before comparison. We've kept the
+      // product names all lower case to keep things simple
+      const lowerCaseSearchTerm = searchTerm.value.trim().toLowerCase();
+      // Filter finalGroup to contain only products whose name includes the search term
+      finalGroup = categoryGroup.filter( product => product.name.includes(lowerCaseSearchTerm));
+    }
+    // Once we have the final group, update the display
+    updateDisplay();
+  }
+
+  // start the process of updating the display with the new set of products
+  function updateDisplay() {
+    // remove the previous contents of the <main> element
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+
+    // if no products match the search term, display a "No results to display" message
+    if (finalGroup.length === 0) {
+      const para = document.createElement('p');
+      para.textContent = 'No results to display!';
+      main.appendChild(para);
+    // for each product we want to display, pass its product object to fetchBlob()
+    } else {
+      for (const product of finalGroup) {
+        fetchBlob(product);
+      }
+    }
+  }
+
+  // fetchBlob uses fetch to retrieve the image for that product, and then sends the
+  // resulting image display URL and product object on to showProduct() to finally
+  // display it
+  function fetchBlob(product) {
+    // construct the URL path to the image file from the product.image property
+    const url = `images/${product.image}`;
+    // Use fetch to fetch the image, and convert the resulting response to a blob
+    // Again, if any errors occur we report them in the console.
+    fetch(url)
+      .then( response => {
         if (!response.ok) {
-            throw new Error("HTTP Error: " + response.status);
+          throw new Error(`HTTP error: ${response.status}`);
         }
         return response.blob();
-    })
-    .then (blob => {
-        const url = URL.createObjectURL(blob);
-        img.src = url;
-    })
-    .catch(error => {
-        console.log("Fetch problem:" + error.message);
-    })
+      })
+      .then( blob => showProduct(blob, product) )
+      .catch( err => console.error(`Fetch problem: ${err.message}`) );
+  }
 
-    // Set their attributes
-    h2.textContent = product.name;
-    p.textContent =`$${product.price}`;
-    img.setAttribute("alt", `Picture of ${product.name}`);
-    section.setAttribute("class", product.type);
-    
-    // Append new elements
+  // Display a product inside the <main> element
+  function showProduct(blob, product) {
+    // Convert the blob to an object URL — this is basically an temporary internal URL
+    // that points to an object stored inside the browser
+    const objectURL = URL.createObjectURL(blob);
+    // create <section>, <h2>, <p>, and <img> elements
+    const section = document.createElement('section');
+    const heading = document.createElement('h2');
+    const para = document.createElement('p');
+    const image = document.createElement('img');
+
+    // give the <section> a classname equal to the product "type" property so it will display the correct icon
+    section.setAttribute('class', product.type);
+
+    // Give the <h2> textContent equal to the product "name" property, but with the first character
+    // replaced with the uppercase version of the first character
+    heading.textContent = product.name.replace(product.name.charAt(0), product.name.charAt(0).toUpperCase());
+
+    // Give the <p> textContent equal to the product "price" property, with a $ sign in front
+    // toFixed(2) is used to fix the price at 2 decimal places, so for example 1.40 is displayed
+    // as 1.40, not 1.4.
+    para.textContent = `$${product.price.toFixed(2)}`;
+
+    // Set the src of the <img> element to the ObjectURL, and the alt to the product "name" property
+    image.src = objectURL;
+    image.alt = product.name;
+
+    // append the elements to the DOM as appropriate, to add the product to the UI
     main.appendChild(section);
-    section.appendChild(h2);
-    section.appendChild(p);
-    section.append(img);
-
-    allProductSections.push(section);
-}
-
-// Filter by select value and show the items according to filter
-select.addEventListener("change", () => {
-    const filter = select.value.toLowerCase();
-    filterBy(filter, main, allProductSections);
-})
-
-function filterBy(filter, main, allProductSections) {
-    // products to be shown
-    const products = filter === "all" ? allProductSections : 
-    allProductSections.filter(p => p.getAttribute("class").includes(filter));
-    // remove all elements inside of main.
-    main.innerHTML = '';
-
-    products.forEach(p => {
-        main.appendChild(p);
-    })
+    section.appendChild(heading);
+    section.appendChild(para);
+    section.appendChild(image);
+  }
 }
